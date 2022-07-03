c905738f9ae2fdaea6bd0da26bde0d2621dfb68b
keptn <keptn@keptn.sh>
2022-07-03 12:16:03 +0000

    Added resources

--- microserviceA/files/execution.sh
+++ microserviceA/files/execution.sh
@@ -1,15 +1,19 @@
-# echo installing xk6
-# go install go.k6.io/xk6/cmd/xk6@latest
-# echo installed xk6
+echo entering /keptn directory
+cd /keptn
+echo entered /keptn directory
 
-# echo building k6 binary with the Prometheus remote write
-# xk6 build --with github.com/grafana/xk6-output-prometheus-remote@latest 
-# echo build successful
+echo exporting GOPATH
+export GOPATH=/keptn
+echo exported GOPATH to $GOPATH 
 
-# echo using that binary to run k6 performance test
-# ./k6 run --duration 30s --vus 10 /keptn/files/k6_test.js
-# echo k6 run completed
+echo installing xk6
+go install go.k6.io/xk6/cmd/xk6@latest -o 
+echo installed xk6
 
-echo jainan-log start
-cd /keptn/jainam-shah
-echo jainam-log end
+echo building k6 binary with the Prometheus remote write
+$GOPATH/bin/xk6 build --with github.com/grafana/xk6-output-prometheus-remote@latest 
+echo build successful
+
+echo using that binary to run k6 performance test
+./k6 run --duration 30s --vus 10 /keptn/files/k6_test.js
+echo k6 run completed
