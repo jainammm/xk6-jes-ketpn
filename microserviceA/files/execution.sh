a2b7ee6b23d540db0606d527e28c26c6996bd58e
keptn <keptn@keptn.sh>
2022-07-03 13:53:59 +0000

    Added resources

--- microserviceA/files/execution.sh
+++ microserviceA/files/execution.sh
@@ -1,25 +1,29 @@
-echo entering /keptn directory
+echo "entering /keptn directory"
 cd /keptn
-echo entered /keptn directory
+echo "entered /keptn directory"
 
-echo exporting GOPATH
+echo "exporting GOPATH"
 export GOPATH=/keptn/go
 mkdir $GOPATH
-echo exported GOPATH to $GOPATH 
+echo "exported GOPATH to $GOPATH"
 
-echo exporting TMPDIR
+echo "turning GOCACHE off"
+export GOCACHE=off
+
+
+echo "exporting TMPDIR"
 export TMPDIR=/keptn/tmp
 mkdir $TMPDIR
-echo exported TMPDIR to $TMPDIR 
+echo "exported TMPDIR to $TMPDIR" 
 
-echo installing xk6
+echo "installing xk6"
 go install go.k6.io/xk6/cmd/xk6@latest
-echo installed xk6
+echo "installed xk6"
 
-echo building k6 binary with the Prometheus remote write
+echo "building k6 binary with the Prometheus remote write"
 $GOPATH/bin/xk6 build --with github.com/grafana/xk6-output-prometheus-remote@latest 
-echo build successful
+echo "build successful"
 
-echo using that binary to run k6 performance test
+echo "using that binary to run k6 performance test"
 ./k6 run --duration 30s --vus 10 /keptn/files/k6_test.js
-echo k6 run completed
+echo "k6 run completed"
